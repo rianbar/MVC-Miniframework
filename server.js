f1700cb00ef4a1c9ad857e0e54e9d77063d9c278
Rian <rianbarroso596@gmail.com>
2023-01-13 22:32:09 -0300

    added session and flash messages

--- server.js
+++ server.js
@@ -16,11 +16,30 @@ mongoose.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifie
         console.log(e);
     });
 
+const session = require('express-session');
+const mongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+
 //libera acesso de métodos post
 app.use(express.urlencoded({ extended: true }))
 
 //setando arquivos estáticos!
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+//setando as sessões
+const sessionOptions = session({
+    secret: '...',
+    store: mongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+})
+
+app.use(sessionOptions);
+app.use(flash());
 
 //setando caminho das views: existem maneiras mais fáceis de trablhar sem usar o path!
 app.set('views', path.resolve(__dirname, 'src', 'views'));
